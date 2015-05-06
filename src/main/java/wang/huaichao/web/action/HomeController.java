9cda3f4238f6416f74acf0321e3124208e63b1f6
doodlecoge@163.com <doodlecoge@163.com>
2015-05-06 11:14:03 +0800

    fix response message type issue

--- src/main/java/wang/huaichao/web/action/HomeController.java
+++ src/main/java/wang/huaichao/web/action/HomeController.java
@@ -101,7 +101,7 @@ public class HomeController {
         String s = "";
         int len = str.length();
         for (int i = 0; i < len; i++) {
-            s += str.charAt(str.charAt(len - 1 - i));
+            s += str.charAt(len - 1 - i);
         }
         return s;
     }
